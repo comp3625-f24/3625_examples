9c2a0330e07d433e2b67c5f218fe819b316438fe
echalmers <echalmers@mtroyal.ca>
2024-11-04 09:48:10 -0700

    add kde

--- comp3625_examples/unsupervised_learning/kernel_density_estimation1.py
+++ comp3625_examples/unsupervised_learning/kernel_density_estimation1.py
@@ -5,8 +5,15 @@ import numpy as np
 
 
 # read in same grades
-data = pd.read_csv('COMP 1701 grades.csv', index_col=None)
+data = pd.read_csv('./comp3625_examples/unsupervised_learning/COMP 1701 grades.csv', index_col=None)
 
 # fit a kernel density estimator
 kde = KernelDensity(bandwidth=0.05)
 kde.fit(X=data['Midterm 1'].to_numpy().reshape((-1, 1)))
+
+# plot the learned distribution
+grade_values = np.linspace(0, 1, 50)
+log_likelihoods = kde.score_samples(grade_values.reshape((-1, 1)))
+
+plt.plot(grade_values, np.exp(log_likelihoods))
+plt.show()
